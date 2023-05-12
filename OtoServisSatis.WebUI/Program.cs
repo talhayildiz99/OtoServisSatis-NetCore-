6df1661f95f5943525166e90eb20cf217035aef5
Talha <99talhayildiz@gmail.com>
2023-05-12 14:38:13 +0300

    Admin Layout

--- OtoServisSatis.WebUI/Program.cs
+++ OtoServisSatis.WebUI/Program.cs
@@ -29,6 +29,11 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+           name: "admin",
+           pattern: "{area:exists}/{controller=Main}/{action=Index}/{id?}"
+         );
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
