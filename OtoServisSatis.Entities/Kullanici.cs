a6a69d47cbf6bba646ab1a365b5821b9d5965d7c
Talha <99talhayildiz@gmail.com>
2023-05-16 16:20:28 +0300

    Completed "Users Control"

--- OtoServisSatis.Entities/Kullanici.cs
+++ OtoServisSatis.Entities/Kullanici.cs
@@ -11,19 +11,25 @@ namespace OtoServisSatis.Entities
     {
         public int Id { get; set; }
         [StringLength(50)]
+        [Display(Name = "Adı")]
         public string Adi { get; set; }
         [StringLength(50)]
+        [Display(Name = "Soyadı")]
         public string Soyadi { get; set; }
         [StringLength(50)]
         public string Email { get; set; }
         [StringLength(20)]
-        public string Telefon { get; set; }
+        public string? Telefon { get; set; }
         [StringLength(50)]
+        [Display(Name = "Kullanıcı Adı")]
         public string KullaniciAdi { get; set; }
         [StringLength(50)]
+        [Display(Name = "Şifre")]
         public string Sifre { get; set; }
         public bool AktifMi { get; set; }
+        [Display(Name ="Eklenme Tarihi"), ScaffoldColumn(false)]
         public DateTime? EklenmeTarihi { get; set; } = DateTime.Now;
+        [Display(Name = "Rol")]
         public int RolId { get; set; }
         public virtual Rol? Rol { get; set; }
     }
